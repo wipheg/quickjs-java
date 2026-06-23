dbaab6dbb3f945abf10010ef62eb9d9e2eb346f5
Mike Bremford <mike@bfo.co.uk>
2026-06-23 15:56:22 +0000

    Build with linty

--- build-jar.sh
+++ build-jar.sh
@@ -8,6 +8,8 @@ javac --class-path ${CLASSPATH}:${WASM_TARGET_RESOURCES} \
       -d target/classes \
       --source-path src/main/java \
       --source-path ${WASM_TARGET_SOURCES} \
+      -Xlint:unchecked \
+      -Xlint:rawtypes \
       src/main/java/com/bfo/quickjs/*.java
 
 jar --create --file ${JAR_FILE} -C target/classes com -C ${WASM_TARGET_RESOURCES} com
